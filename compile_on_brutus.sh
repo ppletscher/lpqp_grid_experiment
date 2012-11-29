781cee4587d2c5f151a29418db098fad9a1e31e9
Patrick Pletscher <pletscher@inf.ethz.ch>
2012-11-29 13:29:31 +0100

    change compile flags

--- compile_on_brutus.sh
+++ compile_on_brutus.sh
@@ -1,2 +1,2 @@
-mcc -R singleCompThread -R nodisplay -R nojvm -m grid_experiment_infer.m -a lpqp/ -a mplp/ -a matluster/
+mcc -R -singleCompThread -R -nodisplay -R -nojvm -m grid_experiment_infer.m -a lpqp/ -a mplp/ -a matluster/
 sed -i '7i\export MCR_CACHE_ROOT=${TMPDIR};' run_grid_experiment_infer.sh
